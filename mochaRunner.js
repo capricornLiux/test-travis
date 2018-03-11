b8298aea08a03e57fa11fc6f06aef892bfcc45e6
liuxin <784532927@qq.com>
2018-03-11 19:46:52 +0800

    整合测试命令2

--- mochaRunner.js
+++ mochaRunner.js
@@ -2,13 +2,12 @@ var Mocha = require('mocha');
 var mocha = new Mocha({
     reporter: 'mochawesome',
     reporterOptions: {
-        reportDir:'./reports/mochawesome-report'
+        reportDir: './reports/mochawesome-report'
     }
-  });
-  mocha.addFile(
-    './test/routeSpec.js'
-);
-mocha.run(function() {
+});
+mocha.addFile('./add.test.js');
+mocha.addFile('./test/routeSpec.js');
+mocha.run(function () {
     console.log('All done');
     process.exit();
 });
